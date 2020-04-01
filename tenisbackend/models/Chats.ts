d13993d14fc71bf151a24543717083a11c467f6c
Julen Garralaga <univideos8@gmail.com>
2020-04-01 16:57:50 +0200

    borrar tenis frontend

--- tenisbackend/models/Chats.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import mongoose = require("mongoose");
-let Schema = mongoose.Schema;
-let chats = new Schema({
-    usuarios: { type: mongoose.Types.ObjectId, ref: 'usuarios' }, //los usuarios que hay en el chat
-    mensajes: [{ type: mongoose.Types.ObjectId, ref: 'mensajes' }] //relacionala id de cada mensaje enviado
-});
-module.exports = mongoose.model('chats', chats);

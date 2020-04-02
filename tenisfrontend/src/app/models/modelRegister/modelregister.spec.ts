eb970a7e7d356dd4bf9763e72dc62c30b7994a9d
Julen Garralaga <univideos8@gmail.com>
2020-04-02 18:40:21 +0200

    quitar front

--- tenisfrontend/src/app/models/modelRegister/modelregister.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Modelregister } from './modelregister';
-
-describe('Modelregister', () => {
-  it('should create an instance', () => {
-    expect(new Modelregister()).toBeTruthy();
-  });
-});

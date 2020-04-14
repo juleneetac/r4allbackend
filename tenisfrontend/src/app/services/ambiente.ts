3e381c40d883232b9524eabb2c9643623ac0e01b
Julen Garralaga <univideos8@gmail.com>
2020-04-14 22:53:44 +0200

    quitar el frontend del repositorio del backend

--- tenisfrontend/src/app/services/ambiente.ts
+++
@@ -1,8 +0,0 @@
-export class Ambiente{
-    urlUsuario = 'http://localhost:7000/usr';  //deben ser las mismas rutas creadas
-    urlTorneo = 'http://localhost:7000/trn';  //en el backend
-    urlPartida = 'http://localhost:7000/prd';
-    urlParticipantest = 'http://localhost:7000/prantes';
-    urlMensaje = 'http://localhost:7000/msg';
-    urlChat = 'http://localhost:7000/cht';
-}
